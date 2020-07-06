54ea9303b3e969b9037ec382d02cad0484065333
sunakan <sunakan03@gmail.com>
2020-07-06 04:25:45 +0000

    シングルスレッドwebサーバ

--- web/src/main.rs
+++ web/src/main.rs
@@ -1,11 +1,13 @@
 use std::io::prelude::*;
 use std::net::TcpStream;
 use std::net::TcpListener;
+use std::fs::File;
 
 fn main() {
     let listener = TcpListener::bind("0.0.0.0:7878").unwrap();
     for stream in listener.incoming() {
         let stream = stream.unwrap();
+        println!("----streamの中身----->{:?}", stream);
         handle_connection(stream);
     }
 }
@@ -13,5 +15,19 @@ fn main() {
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 512];
     stream.read(&mut buffer).unwrap();
-    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+    let get = b"GET / HTTP/1.1\r\n";
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
+    };
+    let mut file = File::open(filename).unwrap();
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+    let response = format!("{}{}", status_line, contents);
+    // 最小のresponse
+    //let response = "HTTP/1.1 200 OK\r\n\r\nhogefuga";
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+    println!("Requestの中身: {}", String::from_utf8_lossy(&buffer[..]));
 }
